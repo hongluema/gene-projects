2c8900027c40730219efdd9ce78a886f5467a315
honglue.mhl <honglue.mhl@gongdao.com>
2025-11-27 11:43:58 +0800

    feat: ocr

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export const USE_MOCK = true
 // TODO: 测试环境
-export const API_BASE = 'http://localhost:8002'
+// export const API_BASE = 'http://localhost:8002'
 // TODO: 生产环境
-// export const API_BASE = 'https://app.oriomics.cn'
+export const API_BASE = 'https://app.oriomics.cn'
 
 export const API = {
   // 认证相关
